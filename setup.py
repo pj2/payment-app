15966171d1e352f5310f7c09d8f89b5048653d49
Joshua Prendergast <j.prendo10@gmail.com>
2016-10-22 12:44:47 +0100

    project: rename project directory to 'common'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from setuptools import setup, find_packages
 
 __version__ = None
-execfile('payments/payments/__init__.py') # Get __version__
+execfile('site/common/__init__.py') # Get __version__
 
 if __version__ is None:
     raise RuntimeError('__version__ not found')
